9d8becdec18c0b976a9dee97fc28f70e39951c70
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-06-01 13:06:37 -0500

    made cool dice game.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,50 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/resume', function()
+{
+    return "This is my resume!";
+});
+
+Route::get('/portfolio', function()
+{
+    return "This is my portfolio!";
+});
+
+/* If you want to have a variable injected into your route, Format is {variable?}.
+Format for default variable is function($name = default variable)
+*/
+Route::get('/sayhello/{name?}', function($name = 'Codeup')
+{
+    if ($name == "Chris") {
+    	return Redirect::to('/');
+    } else {
+    	return "Hello, $name!";
+    }
+});
+
+Route::get('/rolldice/{guess?}', function($guess = 'null')
+{
+    if ($guess == 'null') {
+    	$guess = "You haven't made a guess yet!";
+    	$number = null;
+    	$outcome = null;
+    } else {
+    	$guessNum = $guess;
+    	$guess = "You guessed: $guessNum";
+    	$number = "The dice says: " . mt_rand(1, 6);
+	    if ($guessNum == $number) {
+	    	$outcome = "Great guess!";
+	    } else {
+	    	$outcome = "Wrong...try again!";
+	    }
+    }
+
+    $data = [
+    	'number' => $number,
+    	'guess' => $guess,
+    	'outcome' => $outcome
+	];
+    return View::make('roll-dice')->with($data);
+});
---
+++ app/views/roll-dice.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dice Game</title>
+</head>
+<body>
+    <h1>Dice Game</h1>
+    <p><?= $guess ?></p>
+    <p><?= $number ?> </p>
+    <p><?= $outcome ?> </p>
+    <a href="/rolldice/1">Guess 1</a>
+    <a href="/rolldice/2">Guess 2</a>
+    <a href="/rolldice/3">Guess 3</a>
+    <a href="/rolldice/4">Guess 4</a>
+    <a href="/rolldice/5">Guess 5</a>
+    <a href="/rolldice/6">Guess 6</a>
+</body>
+</html>
