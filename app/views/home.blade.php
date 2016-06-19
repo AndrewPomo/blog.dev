04ed8fd1915a031929349171a093e55f7d7c3196
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-06-19 13:53:22 -0500

    fixed spacing on connect links

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -30,10 +30,10 @@
         <hr id="bigger-margin">
         <div id="links" class="text-center">
         	<h2 id="linksintro">Connect with me:</h2>
-        	<a href={{{'mailto:andrewpomo815@gmail.com'}}}><i class="fa fa-envelope-square fa-4" aria-hidden="true"></i></a>
-        	<a href="https://www.linkedin.com/in/andrewpomo"><i class="fa fa-linkedin-square fa-4" aria-hidden="true"></i></a>
-        	<a href="https://github.com/AndrewPomo"><i class="fa fa-github fa-4" aria-hidden="true"></i></a>
-        	<a href="https://angel.co/andrew-powell-morse"><i class="fa fa-angellist fa-4" aria-hidden="true"></i></a>
+        	<a class="col-xs-6 col-md-4" href={{{'mailto:andrewpomo815@gmail.com'}}}><i class="fa fa-envelope-square fa-4" aria-hidden="true"></i></a>
+        	<a class="col-xs-6 col-md-4" href="https://www.linkedin.com/in/andrewpomo"><i class="fa fa-linkedin-square fa-4" aria-hidden="true"></i></a>
+        	<a class="col-xs-6 col-md-4" href="https://github.com/AndrewPomo"><i class="fa fa-github fa-4" aria-hidden="true"></i></a>
+        	<a class="col-xs-6 col-md-4" href="https://angel.co/andrew-powell-morse"><i class="fa fa-angellist fa-4" aria-hidden="true"></i></a>
         </div>
         <p></p>
     </div>
